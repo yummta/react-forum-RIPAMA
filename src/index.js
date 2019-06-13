4ab13a8da60e01d3bc12a6cd6e039eecfd18d772
Ricardo <ry.yrupailla@gmail.com>
2019-06-13 16:16:17 -0500

    Yarn install

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Login from "./views/Login";
 
-
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-
+ReactDOM.render(<Login />, document.getElementById("root"));
---
+++ src/views/login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <form onSubmit={}>
+        
+        <input type="text" label="usuario"/>
+        <input type="text" label="email"/>
+
+
+      </form>
+    </div>
+  )
+}
+
+export default Login
