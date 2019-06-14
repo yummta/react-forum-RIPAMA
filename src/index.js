ff55862bee317710e67e27996e8ce0c98c12a0f2
Ricardo Yrupailla Meza <ry.yrupailla@gmail.com>
2019-06-14 11:40:28 -0500

    Feature discussion view (#16)
    * Add basic component
    
    * Add emotion core dependency
    
    * Add comment component
    
    * Change format of comment component
    
    * Add text area for comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Login from "./views/login";
-import Discussion from "./views/discussion";
-import Default from "./components/default";
 
 ReactDOM.render(<App />, document.getElementById("root"));
