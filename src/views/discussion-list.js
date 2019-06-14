63c18653ecbdc53d711bec8614404b0f5d63aadd
paul <yummta@gmail.com>
2019-06-14 17:14:40 -0500

    Get data and render discussion
    - Get data from localStorage
    - Temporary use fake data
    - Show discussion in discussion-list

--- src/views/discussion-list.js
+++ src/views/discussion-list.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-
-
-function DiscussionList() {
-
-
-  return (
-    <section>
-      {[1, 2, 3].map((el, index) => (
-        <article key={index}>
-          <hr />
-          <Link to="/discussions/detashe">
-            <h2>Super interesting title about things that I liked</h2>
-          </Link>
-          <span>Paul Portillo</span>
-        </article>
-      ))}
-    </section>
-  );
+function DiscussionList({ discussions }) {
+  if (discussions.length) {
+    return (
+      <section>
+        {discussions.map(discussion => (
+          <article key={discussion.id}>
+            <hr />
+            <Link to={`/discussions/${discussion.id}`}>
+              <h2>{discussion.title}</h2>
+            </Link>
+            <span>{discussion.author}</span>
+          </article>
+        ))}
+      </section>
+    );
+  } else {
+    return <p>Sorry. We do not messages!</p>;
+  }
 }
 
 export default DiscussionList;
