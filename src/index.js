4b22eeb04b25bc1d93843ba46f47f9129c685afd
Ricardo Yrupailla Meza <ry.yrupailla@gmail.com>
2019-06-14 10:10:01 -0500

    Create default component (#13)
    * Create default component
    
    * Change format default component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Login from "./views/login";
 import Discussion from "./views/discussion";
+import Default from "./components/default";
 
 ReactDOM.render(<App />, document.getElementById("root"));
