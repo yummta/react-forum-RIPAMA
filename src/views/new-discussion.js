73d8054a7898f22839ce086f8d092c1d465f3e4e
mayra navarro <luciagirasoles@gmail.com>
2019-06-15 23:49:10 -0500

    add css in new discussion view

--- src/views/new-discussion.js
+++ src/views/new-discussion.js
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import React from "react";
 import { Redirect } from "@reach/router";
 
@@ -30,29 +32,62 @@ function NewDiscussion({ discussions, setDiscussions, author }) {
 
   if (!newId) {
     return (
-      <React.Fragment>
-        <form onSubmit={handleSubmit}>
-          <h1>New Discussion</h1>
-          <label>
-            Title
-            <br />
-            <input
-              type="text"
-              onChange={handleChange}
-              name="title"
-              autoComplete="off"
-            />
-          </label>
-          <label>
-            Body
-            <br />
-            <textarea onChange={handleChange} name="body" />
-          </label>
-          <button type="submit" name="submit" value="Submit">
+      <div
+        css={{
+          padding: "20px",
+          display: "flex"
+        }}
+      >
+        <form
+          onSubmit={handleSubmit}
+          css={{
+            display: "flex",
+            flexDirection: "column",
+            width: "400px",
+            boxShadow: "0 20px 25px -5px rgba(0, 0, 0, 0.5)",
+            padding: "1em",
+            background: "rgba(22, 22, 22, 0.57)",
+            borderRadius: "0.5em",
+            "& *": { fontFamily: "Arial", fontSize: "larger" }
+          }}
+        >
+          <h1>
+            <strong>New Discussion</strong>
+          </h1>
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            type="text"
+            onChange={handleChange}
+            name="title"
+            autoComplete="off"
+          />
+
+          <label htmlFor="body">Body </label>
+          <textarea id="body" onChange={handleChange} name="body" />
+
+          <button
+            type="submit"
+            name="submit"
+            css={{
+              backgroundColor: "black",
+              border: "none",
+              color: "white",
+              padding: "15px 32px",
+              textAlign: "center",
+              textDecoration: "none",
+              display: "inline-block",
+              fontSize: "larger",
+              margin: "4px 2px",
+              cursor: "pointer",
+              marginTop: "0.5em",
+              ":hover": { backgroundColor: "white", color: "black" }
+            }}
+          >
             Submit
           </button>
         </form>
-      </React.Fragment>
+      </div>
     );
   }
   return <Redirect to={`/discussions/${newId}`} noThrow />;
